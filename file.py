e6656f2276aad93bf78470a8ad03623ea7ede220
FloatingInt <77575424+Floating-Int@users.noreply.github.com>
2022-02-21 22:12:50 +0100

    Initial commit

---
+++ file.py
@@ -0,0 +1,16 @@
+from lib1 import func
+from lib2 import sub
+from os import system
+import sys
+import moviepy
+#from package.packagefile import packagefunc
+#import package
+# from ..PackagePhobia.package.packagefile import packagefunc  # import with '..'
+import tkinter
+import collections
+
+
+if __name__ == "__main__":
+    x = sub(3, 1)
+    print(func() + x)  # >>> 5
+    print("Testing func:", packagefunc)
